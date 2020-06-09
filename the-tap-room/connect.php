854de5e3489faf1f9aea7c303f41e1272232e2c6
Andrew Burt <andrew.douglas.burt@gmail.com>
2020-06-09 14:48:02 -0700

    Uploaded source

---
+++ the-tap-room/connect.php
@@ -0,0 +1,11 @@
+<?php
+define(HOST, '*redacted*');
+define(USERNAME, '*redacted*');
+define(PW, '*redacted*');
+define(DB, '*redacted*');
+define(IMG_DIR,'./images/');
+$db = new mysqli(HOST,USERNAME, PW, DB); 
+
+?>
+
+
